bc1f0b7cde3529da4b23f918e182c6bd07d4a7a1
Abhijeet <abhi@botzee.com>
2025-01-18 13:32:33 +0530

    added quiz app

---
+++ Event_delgation/quiz/script.js
@@ -0,0 +1,29 @@
+const correctAnswers = ["D", "B", "C", "B", "D"];
+const quizForm = document.querySelector(".quiz-form");
+const questions = document.querySelectorAll(".question");
+const result = document.querySelector(".result");
+
+quizForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const userAnswers = [
+    quizForm.q1.value,
+    quizForm.q2.value,
+    quizForm.q3.value,
+    quizForm.q4.value,
+    quizForm.q5.value,
+  ];
+  let score = 0;
+
+  userAnswers.forEach(function (userAnswer, index) {
+    if (userAnswer === correctAnswers[index]) {
+      score++;
+      questions[index].classList.add("correct");
+    } else {
+      questions[index].classList.add("wrong");
+    }
+  });
+
+  result.classList.remove("hide");
+  result.querySelector(".score-result").innerText = `You Scored ${score}/5`;
+});
