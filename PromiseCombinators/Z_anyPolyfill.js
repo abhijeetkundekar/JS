752b997b3997503ad24f7f5b1b5acb5d941feb40
Abhijeet <abhi@botzee.com>
2025-01-29 17:21:30 +0530

    Promise any polyfill

--- PromiseCombinators/Z_anyPolyfill.js
+++ PromiseCombinators/Z_anyPolyfill.js
@@ -1,6 +1,6 @@
 function resolveQuickly() {
   return new Promise((resolve) => {
-    setTimeout(() => resolve("Resolved quickly"), 300);
+    setTimeout(() => resolve("Resolved quickly"), 3000);
   });
 }
 
@@ -22,7 +22,27 @@ function rejectSlowly() {
   });
 }
 
-Promise.any([rejectFast(), rejectSlowly(), resolveQuickly(), resolveSlowly()])
+Promise.myAny = function (values) {
+  return new Promise((resolve, reject) => {
+    if (values.length == 0)
+      reject(new AggregateError("No Promises were provided"));
+
+    let rejectArr = [];
+    let rejectCount = 0;
+    values.forEach((item, index) => {
+      Promise.resolve(item)
+        .then(resolve)
+        .catch((error) => {
+          rejectArr[index] = error;
+          rejectCount++;
+
+          if (rejectCount === values.length)
+            reject(new AggregateError(rejectArr, "All Promises were Rejected"));
+        });
+    });
+  });
+};
+Promise.myAny([rejectFast(), rejectSlowly(), resolveQuickly(), resolveSlowly()])
   .then((result) => {
     console.log("Result:", result);
   })
